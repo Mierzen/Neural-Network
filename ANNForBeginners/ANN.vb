3f57d6bdf2ca1a0442243ba523ce75cf220c4951
Mierzen <steptaljaard@gmail.com>
2015-03-28 12:24:06 +0200

    Add ANN module

---
+++ ANNForBeginners/ANN.vb
@@ -0,0 +1,23 @@
+﻿Module ANN
+    'Const numInputs As Integer = 6
+    'Const numOutputs As Integer = 2
+    'Const numLayers As Integer 'num hidden layers. Total layers - inputL - outputL
+
+
+
+    Function neuronSum(ByVal inputs() As Double, ByVal weights As Double()) As Double
+        If inputs.Length <> weights.Length Then
+            MsgBox("Input array and weight array not equal lenght")
+            Return 0
+        End If
+
+        Dim sum As Double = 0
+
+        For i = 0 To inputs.Length - 1
+            sum += inputs(i) * weights(i)
+        Next
+
+        Return sum
+    End Function
+
+End Module
--- ANNForBeginners/Form1.vb
+++ ANNForBeginners/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim inputs() As Double = {1, 0.25, -0.5}
+        Dim weights() As Double = {0.5, 0.5, 0.5}
+
+        MsgBox(ANN.neuronSum(inputs, weights))
+
+    End Sub
 End Class

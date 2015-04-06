fd5e8f40dce14bb8ceba43faf1587d1f05c1101f
Mierzen <steptaljaard@gmail.com>
2015-04-06 14:25:02 +0200

    Add Util module

---
+++ ANNForBeginners/Util.vb
@@ -0,0 +1,13 @@
+﻿Module Util
+    Public Function GetRow(rowNumber As Integer, array As Double(,))
+        Dim arrayParameterCount As Integer = array.GetLength(1)
+
+        Dim returnArray(arrayParameterCount - 1) As Double
+
+        For i = 0 To arrayParameterCount - 1
+            returnArray(i) = array(rowNumber, i)
+        Next
+
+        Return returnArray
+    End Function
+End Module

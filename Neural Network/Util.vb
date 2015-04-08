147909f176d95a4916c5a289529325ca13700938
Stéphan Taljaard <steptaljaard@gmail.com>
2015-04-08 11:07:57 +0200

    Add better random function to Util class: Gaussian

--- Neural Network/Util.vb
+++ Neural Network/Util.vb
@@ -12,4 +12,38 @@
             Return returnArray
         End Function
     End Class
+
+    Public NotInheritable Class Random
+        Private Sub New()
+        End Sub
+        Private Shared gen As New System.Random()
+
+        Public Shared Function Gaussian() As Double
+            Return Gaussian(0.0, 1.0)
+        End Function
+
+        Public Shared Function Gaussian(mean As Double, stddev As Double) As Double
+            Dim rVal1 As Double
+            Dim rVal2 As Double
+
+            Gaussian(mean, stddev, rVal1, rVal2)
+
+            Return rVal1
+        End Function
+
+        Public Shared Sub Gaussian(mean As Double, stddev As Double, ByRef val1 As Double, ByRef val2 As Double)
+            Dim u As Double, v As Double, s As Double, t As Double
+
+            Do
+                u = 2 * gen.NextDouble() - 1
+                v = 2 * gen.NextDouble() - 1
+            Loop While u * u + v * v > 1 OrElse (u = 0 AndAlso v = 0)
+
+            s = u * u + v * v
+            t = Math.Sqrt((-2.0 * Math.Log(s)) / s)
+
+            val1 = stddev * u * t + mean
+            val2 = stddev * v * t + mean
+        End Sub
+    End Class
 End Class

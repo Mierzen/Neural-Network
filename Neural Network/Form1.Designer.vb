bc951fbfd46c18fe8aca13ef3c3c5ec8929b36bd
Stéphan Taljaard <steptaljaard@gmail.com>
2015-04-10 20:13:09 +0200

    Add and use icon

--- Neural Network/Form1.Designer.vb
+++ Neural Network/Form1.Designer.vb
@@ -29,6 +29,7 @@ Partial Class Form1
         Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
         Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Dim Title1 As System.Windows.Forms.DataVisualization.Charting.Title = New System.Windows.Forms.DataVisualization.Charting.Title()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.tc_modes = New System.Windows.Forms.TabControl()
         Me.tp_training = New System.Windows.Forms.TabPage()
@@ -895,6 +896,7 @@ Partial Class Form1
         Me.Controls.Add(Me.panel_stats)
         Me.Controls.Add(Me.panel_charts)
         Me.Controls.Add(Me.tc_modes)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Padding = New System.Windows.Forms.Padding(0, 0, 8, 8)
